12f3d318aab9e571c9e27d6fb601854d6609b975
SuryaSanjana <sanjanaailavarapu26@gmail.com>
2021-10-16 15:24:49 +0530

    project setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+// // Importing Env Variables
+// require("dotenv").config();
+
+// Libraries
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+const zomato = express();
+zomato.use(helmet());
+zomato.use(cors());
+
+zomato.get("/",(req,res)=>res.json({message:"Setup Success"}));
+
+zomato.listen(4000,()=>console.log("Server is running"));
